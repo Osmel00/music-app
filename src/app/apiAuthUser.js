ff3078fa3f636d3f1b04d7e029c94915b3e33823
Osmel00 <osmel.fauregonzalez@dci.education>
2023-05-21 21:03:30 +0200

    commit

--- src/app/apiAuthUser.js
+++ src/app/apiAuthUser.js
@@ -3,8 +3,8 @@ const API_SERVER = process.env.REACT_APP_MY_API_SERVER;
 export const apiAuthUsers = createApi({
   reducerPath: "apiAuthUsers",
   baseQuery: fetchBaseQuery({
-    baseUrl: "http://localhost:8000/api/v1/auth/", 
-     //baseUrl: "https://api-rest-node-express.up.railway.app/api/v1/auth/" ,//https://api-rest-node-express.up.railway.app
+     //baseUrl: "http://localhost:8000/api/v1/auth/", 
+     baseUrl: "https://api-rest-node-express.up.railway.app/api/v1/auth/" ,//https://api-rest-node-express.up.railway.app
     credentials: "include",
   }),
   
